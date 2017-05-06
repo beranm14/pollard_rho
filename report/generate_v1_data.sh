76c9089cf1e2d5677b3d610e5ee905df9161122c
Martin Beránek <martin.beranek@ecommerceholding.cz>
2017-05-06 21:58:06 +0200

    First version of report

---
+++ report/generate_v1_data.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+i=1;
+
+out="data_v1.txt";
+echo -n > $out;
+
+while [[ $i -le 1024 ]];do
+	j=1;
+	while [[ $j -le 1024 ]];do
+		drl="../benchmarks_v1/logs_gtx_780_Ti_pol/log_${i}_${j}/";
+		if [[ -d $drl ]];then
+			tml=`grep "Time: " ${drl}gpu_job_del.sh.o* | cut -d' ' -f 2`;
+			echo $i $j $tml;
+			[[ ! -z "$tml" ]] && { echo $i $j $tml >> $out; };
+		fi
+		j=$(($j+1));
+	done;
+	i=$(($i+1));
+done;
+
